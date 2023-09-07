08a43a5e4100848f32cf514b99f33ddcadac6b61
vanessaokumu <okumuvanessa@gmail.com>
2023-09-07 09:05:36 -0700

    pack web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,12 +10,11 @@ def do_pack():
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
     
-    try:
-        local("sudo mkdir -p versions")
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = "versions/web_static_{}.tgz".format(date)
-        result = local("sudo tar -cvzf {} web_static".format(filename))
-        if result.succeeded:
-            return filename
-    except:
-            return None
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
