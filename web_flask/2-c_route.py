0a627717a4170c876244f5ad3df322c775062bca
vanessaokumu <okumuvanessa@gmail.com>
2023-09-25 00:36:12 -0700

    2-c route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-from flask import Flask, escape
 
+#!/usr/bin/python3
+"""a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+# Create a Flask application
 app = Flask(__name__)
 
-# Define a route for the root URL ("/") 
+# Route to display "Hello HBNB!"
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
-# Define a route for "/hbnb" 
+# Route to display "HBNB"
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
-# Define a route for "/c/<text>"
+# Route to display "C " followed by the value of the text variable
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    text = escape(text).replace('_', ' ')
+    text = text.replace('_', ' ')
     return "C {}".format(text)
 
+# Run the Flask app on 0.0.0.0, port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
