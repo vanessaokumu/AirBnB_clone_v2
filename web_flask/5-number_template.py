04f439a9832eeb5487acad42777ed4536f951bc4
vanessaokumu <okumuvanessa@gmail.com>
2023-09-25 01:12:41 -0700

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Route to display "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Route to display "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+# Route to display "C ", followed by the value of the text variable
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+# Route to display "Python ", followed by the value of the text variable
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+# Route to display "n is a number" only if n is an integer
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+    else:
+        return "Not a valid number"
+
+# Route to display an HTML page only if n is an integer
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', number=n)
+    else:
+        return "Not a valid number"
+
+# Run the Flask app on 0.0.0.0, port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
