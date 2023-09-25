dc8f456e23b04fdd3acc786f7550d87c9ab91b76
vanessaokumu <okumuvanessa@gmail.com>
2023-09-25 00:29:25 -0700

    updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,14 @@
 """ script that starts a Flask web application """
 from flask import Flask
 
+# Create a Flask application
 app = Flask(__name__)
 
+# Define a route for the root URL ("/")
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+# Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) #Listening on port 5000
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,6 +5,7 @@ Your web application must be listening on 0.0.0.0, port 5000 """
 
 from flask import Flask
 
+# Create a Flask application
 app = Flask(__name__)
 
 # Route for the root URL ("/")
